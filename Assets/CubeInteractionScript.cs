75c2e859b73d3a41a846d799f3f6f6c254a13103
Shamsul Arafin Mahtab <arafinmahtab@gmail.com>
2017-12-08 03:57:41 +0600

    added new 360 player

---
+++ Assets/CubeInteractionScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeInteractionScript : MonoBehaviour {
+
+	public GameObject gameObjectForCube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PointerDown() {
+	//	Camera.main.transform.Translate(300,0,0);
+	//	transform.position = new Vector3 (300, 0, 0);
+		Debug.Log ("Pointer Down");
+
+	}
+}
