10c5c1c48de7365711f3c16cc5f85325a2647fa0
Shamsul Arafin Mahtab <arafinmahtab@gmail.com>
2017-12-14 16:37:12 +0600

    added pointer integration

--- Assets/CubeInteractionScript.cs
+++ Assets/CubeInteractionScript.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class CubeInteractionScript : MonoBehaviour {
 
 	public GameObject gameObjectForCube;
+	public GameObject gameObjectForCube2;
+//	Vector3 tempPosi;
+	Vector3 tempPosi, tempPosi2;
 
 	// Use this for initialization
 	void Start () {
-		
+		tempPosi = gameObjectForCube.transform.position;
+		tempPosi2 = tempPosi;
 	}
 	
 	// Update is called once per frame
@@ -17,9 +21,14 @@ public class CubeInteractionScript : MonoBehaviour {
 	}
 
 	public void PointerDown() {
-	//	Camera.main.transform.Translate(300,0,0);
-	//	transform.position = new Vector3 (300, 0, 0);
+//		Camera.main.transform.Translate(300,0,0);
+//		transform.position = new Vector3 (300, 0, 0);
 		Debug.Log ("Pointer Down");
 
+		tempPosi.x = 300f;
+
+		gameObjectForCube.transform.position = tempPosi;
+		gameObjectForCube2.transform.position = tempPosi2;
+		transform.position = tempPosi;
 	}
 }
